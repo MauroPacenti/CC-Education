9e299247ebcc698ca23d2e83677266eae2d3429f
Trinal <mauro_pacenti@hotmail.it>
2024-11-23 11:05:25 +0100

    Spring boot project created.

---
+++ src/main/java/com/example/cceducation/CcEducationApplication.java
@@ -0,0 +1,13 @@
+package com.example.cceducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cceducation/CcEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cceducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
