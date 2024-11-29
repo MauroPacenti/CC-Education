07557db223c745ceaede5e4658dcd8e51649684c
alex <alexander.pelle@edu.itspiemonte.it>
2024-11-29 21:38:33 +0100

    Annotation @Entity added

--- src/main/java/com/novo/entities/Organization.java
+++ src/main/java/com/novo/entities/Organization.java
@@ -1,6 +1,7 @@
 package com.novo.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -8,6 +9,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 @Table(name = "organizations")
 public class Organization {
 
