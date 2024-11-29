5bdcf640f64def0af7ff4ff4ccb39ffb87a9266b
alex <alexander.pelle@edu.itspiemonte.it>
2024-11-29 21:32:06 +0100

    Entity Journey created

---
+++ src/main/java/com/novo/entities/Journey.java
@@ -0,0 +1,39 @@
+package com.novo.entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "journeys")
+public class Journey {
+	
+	@Column(name = "id")
+	@Id
+	private int id;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "annotations")
+	private String annotations;
+	
+	@Column(name = "start_date")
+	private Date start_date;
+	
+	@Column(name = "end_date")
+	private Date end_date;
+	
+	@OneToOne
+	@JoinColumn(name = "journey_request_id", referencedColumnName = "id")
+	private Journey_request journey_request_id;
+
+}
