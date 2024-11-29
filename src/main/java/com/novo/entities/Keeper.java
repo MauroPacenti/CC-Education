11c4861cdd1a31d6d33db3f21cc986f44450722e
alex <alexander.pelle@edu.itspiemonte.it>
2024-11-29 20:59:02 +0100

    Entity Keeper created

---
+++ src/main/java/com/novo/entities/Keeper.java
@@ -0,0 +1,33 @@
+package com.novo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "keepers")
+public class Keeper {
+	
+	@Id
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "first_name")
+	private String first_name;
+	
+	@Column(name = "last_name")
+	private String last_name;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "cf")
+	private String cf;
+	
+	@Column(name = "phone")
+	private String phone;
+
+}
