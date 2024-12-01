f18f845b02e2b4226bf2dbdc379cbd5ea2c0d091
edo <edoardo.berardengo@edu.itspiemonte.it>
2024-12-01 20:09:17 +0100

    JourneyRequest.java
    Created entity JourneyRequest

---
+++ src/main/java/com/novo/entities/InfoRequest.java
@@ -0,0 +1,30 @@
+package com.novo.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "info_requests")
+public class InfoRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Remove or comment this line if needed
+    @Column(name = "id") // "id" column
+    private int id;
+
+    @Column(name = "email") // "email" column
+    private String email;
+
+    @Column(name = "title") // "title" column
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT") // "content" column
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id") // Foreign key for "status_id" column
+    private Status status;
+}
