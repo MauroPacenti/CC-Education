59b16baef3225ba453ff82fc06c188a4c4320b3b
edo <edoardo.berardengo@edu.itspiemonte.it>
2024-12-01 19:52:30 +0100

    status.java
    Created entity status.java

---
+++ src/main/java/com/novo/entities/JourneyRequest.java
@@ -0,0 +1,38 @@
+package com.novo.entities;
+
+import jakarta.persistence.*;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "journey_requests")
+public class JourneyRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Remove or comment this line if needed
+    @Column(name = "id") // "id" column
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "keeper_id")
+    private Keeper keeper; // Foreign key for "keeper_id" column
+
+    @Column(name = "start_availability_date") // "start_availability_date" column 
+    @Temporal(TemporalType.DATE)
+    private Date startAvailabilityDate;
+
+    @Column(name = "end_availability_date") // "end_availability_date" column
+    @Temporal(TemporalType.DATE)
+    private Date endAvailabilityDate;
+
+    @Column(name = "duration") // "duration" column
+    private int duration;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id") // Foreign key for "status_id" column
+    private Status status;
+}
+	
