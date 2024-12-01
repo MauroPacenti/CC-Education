83427a379027407617e15398affc9a1936ce3aea
Trinal <mauro_pacenti@hotmail.it>
2024-12-01 18:19:32 +0100

    Admin entity created

---
+++ src/main/java/com/novo/entities/Admin.java
@@ -0,0 +1,24 @@
+package com.novo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "`admin`")
+public class Admin {
+    @Id
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+}
