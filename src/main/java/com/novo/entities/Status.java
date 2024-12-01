73c535c4b5dd5f297902fb534cf4cc72a99ab24d
edo <edoardo.berardengo@edu.itspiemonte.it>
2024-12-01 20:02:27 +0100

    InfoRequest.java
    Created entity InfoRequest

---
+++ src/main/java/com/novo/entities/Status.java
@@ -0,0 +1,29 @@
+package com.novo.entities;
+
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "statuses")
+public class Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Remove or comment this line if needed
+    @Column(name = "id") // "id" column
+    private int id;
+
+    @Column(name = "name") // "name" column
+    private String name;
+
+    // OneToMany relation with JourneyRequest
+    @OneToMany(mappedBy = "status")
+    private List<JourneyRequest> journeyRequests;
+
+    // OneToMany relation with InfoRequest
+    @OneToMany(mappedBy = "status")
+    private List<InfoRequest> infoRequests;
+}
