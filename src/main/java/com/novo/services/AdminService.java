f74337217c3c1c0ed6244e2687249f7256570ee8
Trinal <mauro_pacenti@hotmail.it>
2024-12-02 12:57:06 +0100

    AdminService.java and AdminServiceImpl.java created

---
+++ src/main/java/com/novo/services/AdminService.java
@@ -0,0 +1,14 @@
+package com.novo.services;
+
+import com.novo.entities.Admin;
+
+public interface AdminService {
+    void saveTemporaryCredentials(String password, String email);
+    void saveNewCredentials();
+    boolean securityCheck(String oldEmailCode, String newEmailCode);
+    Admin getAdmin();
+    boolean validatePassword(String password);
+    void validateEmail(String email);
+    boolean validateUsername(String username);
+
+}
---
+++ src/main/java/com/novo/services/AdminServiceImpl.java
@@ -0,0 +1,73 @@
+package com.novo.services;
+
+import com.novo.entities.Admin;
+import com.novo.repos.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private AdminRepository adminRepo;
+
+
+    @Override
+    public void saveTemporaryCredentials(String newPassword, String newEmail) {
+        Admin temp = adminRepo.findById("temp").get();
+        temp.setPassword(newPassword);
+        temp.setEmail(newEmail);
+        String oldEmail = adminRepo.findById("admin").get().getEmail();
+        if(!newEmail.equals(oldEmail)){
+            //save security codes into security_check and emails old and new adresses
+            //securityActivation(newEmail, oldEmail);
+        }
+        else{
+            //save security codes into security_check and emails admin address
+            //securityActivation(oldEmail);
+        }
+        adminRepo.save(temp);
+    }
+
+    @Override
+    public void saveNewCredentials() {
+        Admin admin = adminRepo.findById("temp").get();
+        admin.setUsername("admin");
+        adminRepo.save(admin);
+    }
+
+    @Override
+    public boolean securityCheck(String oldEmailCode, String newEmailCode) {
+        return false;
+    }
+
+    //Retrieves admin
+    @Override
+    public Admin getAdmin() {
+        return adminRepo.findAll().get(0);
+    }
+
+    //Responsable to validate password changes
+    @Override
+    public boolean validatePassword(String password) {
+        // Regex pattern for validation (at least a number, a special char, an upper char and a lower char)
+        String regex = "(?=.*[0-9])(?=.*[!@#$%^&*.\\-_])(?=.*[A-Z])(?=.*[a-z])[A-Za-z0-9!@#$%^&*.\\-_]{12,}$";
+        Pattern pattern = Pattern.compile(regex);
+
+        return pattern.matcher(password).matches();
+    }
+
+    //Responsable to validate email changes
+    @Override
+    public void validateEmail(String email) {
+        //It must email the new address with a code,
+        //the code must be inserted to confirm the change
+    }
+
+    //Responsable to validate username changes
+    @Override
+    public boolean validateUsername(String username) {
+        return false;
+    }
+}
