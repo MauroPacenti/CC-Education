66e5bb719281625fb1baa5d870cbeddba2a42bef
alex <alexander.pelle@edu.itspiemonte.it>
2024-12-05 11:58:51 +0100

    @Repository added

--- src/main/java/com/novo/repos/GroupRepository.java
+++ src/main/java/com/novo/repos/GroupRepository.java
@@ -2,9 +2,11 @@ package com.novo.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.novo.entities.Group;
 
+@Repository
 public interface GroupRepository extends JpaRepository<Group, Integer> {
 	
 	    // Esegue una query dove conta il numero di keepers all'interno del gruppo
