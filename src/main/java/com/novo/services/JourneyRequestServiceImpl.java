24751b9e6d684c88e12cf1db33c59c43d2289f2a
Trinal <mauro_pacenti@hotmail.it>
2025-01-07 11:52:01 +0100

    JourneyRequestService created and implemented

---
+++ src/main/java/com/novo/services/JourneyRequestServiceImpl.java
@@ -0,0 +1,48 @@
+package com.novo.services;
+
+import com.novo.entities.JourneyRequest;
+import com.novo.entities.Keeper;
+import com.novo.repos.JourneyRequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JourneyRequestServiceImpl implements JourneyRequestService {
+    @Autowired
+    private JourneyRequestRepository journeyRequestRepo;
+
+    @Override
+    public List<JourneyRequest> getJourneyRequests() {
+        return journeyRequestRepo.findAll();
+    }
+
+    @Override
+    public Optional<JourneyRequest> getJourneyRequest(int journeyRequestId) {
+        return journeyRequestRepo.findById(journeyRequestId);
+    }
+
+    @Override
+    public void addJourneyRequest(JourneyRequest journeyRequest) {
+        journeyRequestRepo.save(journeyRequest);
+    }
+
+    @Override
+    public void deleteJourneyRequest(int journeyRequestId) {
+        journeyRequestRepo.deleteById(journeyRequestId);
+    }
+
+    @Override
+    public void updateJourneyRequest(int journeyRequestId, JourneyRequest journeyRequest) {
+        JourneyRequest edited=journeyRequestRepo.findById(journeyRequestId).get();
+        if(journeyRequest.getStartAvailabilityDate() != null)
+            edited.setStartAvailabilityDate(journeyRequest.getStartAvailabilityDate());
+        if(journeyRequest.getEndAvailabilityDate() != null)
+            edited.setEndAvailabilityDate(journeyRequest.getEndAvailabilityDate());
+        if(journeyRequest.getDuration() != 0)
+            edited.setDuration(journeyRequest.getDuration());
+        journeyRequestRepo.save(journeyRequest);
+    }
+}
