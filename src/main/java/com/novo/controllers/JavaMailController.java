436e3d93b3a8efd15eb0974b2a9605bdf12d99d2
MauroPacenti <162309618+MauroPacenti@users.noreply.github.com>
2025-01-27 15:53:07 +0100

    Update JavaMailController.java

--- src/main/java/com/novo/controllers/JavaMailController.java
+++ src/main/java/com/novo/controllers/JavaMailController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class Mailer {
+public class JavaMailController {
     @Autowired
     private JavaMailSenderService javaMailSenderService;
 
